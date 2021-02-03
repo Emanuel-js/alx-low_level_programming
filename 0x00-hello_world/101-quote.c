c53f253e7ff7f2edb6f2320c517d41ee89b52729
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-03 22:53:18 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
-#include <stdio.h> 
-#include <unistd.h> 
+#include <unistd.h>
+
 /**
- * main - Entry point
+ * main - Where the program starts
  *
- * Return: Always 0 (Success)
+ * Return: 1
  */
-int write(int filedes, const char *buf, unsigned int nbyte); 
-int main() 
-{ 
-  write(1, "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",100); 
-  return 1; 
+int main(void)
+{
+	char q[] = "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n";
+
+	write(2, q, 59);
+	return (1);
 }
