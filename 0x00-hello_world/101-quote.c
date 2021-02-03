259e41c04c7ca5d9cc8fc24c3c14e1ba101c65fe
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-03 23:12:51 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <unistd.h>
+
 /**
  * main - Where the program starts
  *
@@ -6,8 +7,9 @@
  */
 int main(void)
 {
-	 char msg*= "and that piece of art is useful\" - \
-                       Dora Korpar, 2015-10-19\n";
-            fputs(msg, stdout);
+	char q[] = "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n";
+
+	write(2, q, 59);
 	return (1);
 }
