de120e554ce8f8c96ccd3844f85e9eb8c63fe34c
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-03 23:09:44 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	 char msg[]  = "and that piece of art is useful\" - \
-Dora Korpar, 2015-10-19\n";
-syscall(SYS_write, 1, msg, sizeof(msg));
+                       Dora Korpar, 2015-10-19\n";
+         syscall(SYS_write, 1, msg, sizeof(msg));
 	return (1);
 }
