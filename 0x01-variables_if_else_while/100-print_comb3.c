c192bc300aae54f4c732bc6093d9ac27cb2704bc
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-04 23:24:47 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,16 +14,11 @@ int main(void)
 	{
 		for (second = first+1; second <= 9; second++)
 		{
-			
+			putchar(first + '0');   
+			putchar(second + '0');
 			if (first==second || first>second)
 			{
 			 return(0);
-			}
-			putchar(first + '0');   
-			putchar(second + '0');
-			if (first == 9 && second == 9)
-			{
-			break;
 			}
 			else
 			{
