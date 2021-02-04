4642b84a62a793cd91af54d8bef80b41eeba2a89
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-04 23:30:53 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,20 +9,20 @@
 
 int main(void)
 {
-int c, t;
-for (c = '0'; c <= '9'; c++)
-{
-for (t = c + 1; t <= '9'; t++)
-{
-putchar(c);
-putchar(t);
-if (c != '8' || t != '9')
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n');
-return (0);
+            int c, t;
+            for (c = '0'; c <= '9'; c++)
+            {
+              for (t = c + 1; t <= '9'; t++)
+              {
+                    putchar(c);
+                    putchar(t);
+                    if (c != '8' || t != '9')
+                    {
+                    putchar(',');
+                    putchar(' ');
+                    }
+              }
+            }
+            putchar('\n');
+            return (0);
 }
