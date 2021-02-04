f4956fda5d7ab621a0939f822918f79578dadf3e
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-04 23:36:44 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,32 +1,35 @@
 #include <stdio.h>
-/**
-*
-* main -Entry point
-*
-*Return: Always 0 (Success)
-*/
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-            int c; 
-            int t;
-            for (c = '0'; c <= '9'; c++)
-            {
-              for (t = c + 1; t <= '9'; t++)
-              {
-                    putchar(c);
-                    putchar(t);
-                    if(c>t || c==t)
-                    { 
-                    return(0);
-                    } 
-                    if (c != '8' || t != '9')
-                    {
-                    putchar(',');
-                    putchar(' ');
-                    }
-              }
-            }
-            putchar('\n');
-            return (0);
+	int num = 0;
+	int div, mod, aux;
+
+	while (num < 100)
+	{
+		div = num / 10;
+		mod = num % 10;
+		aux = mod * 10 + div;
+
+		if (num != aux && num < aux)
+		{
+			putchar((num / 10) + '0');
+			putchar((num % 10) + '0');
+
+			if (num != 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	num++;
+	}
+
+	putchar('\n');
+	return (0);
 }
