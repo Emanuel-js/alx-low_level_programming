25edccf74faa0fb6d4f01a27f9c80fe5b711eee8
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-04 21:59:06 +0300

    Create  6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point for writing entire base10 single digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = '0' ; ch <= '9' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
