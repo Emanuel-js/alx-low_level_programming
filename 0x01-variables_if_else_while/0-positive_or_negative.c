3962982693167021c6bd3cc458f9cbc370c2e7f8
Emanuel-js <etioaman1960@gmail.com>
2021-02-04 16:47:36 +0000

    positive

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*
+* main -Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+  int n;
+
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+  /* your code goes there */
+  return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+c var and loop condtions
