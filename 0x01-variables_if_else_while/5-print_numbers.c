53a967e38b30ee66c3d8ff42f9e1b9a590580358
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-04 21:53:44 +0300

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point for writing entire base10 single digits
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{    int n=0;
+     while(n<10)
+     {
+     printf(n);
+     n++;
+     printf("/n");
+     }
+     
+     return(0)
+}
