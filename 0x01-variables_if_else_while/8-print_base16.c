346ebc21504bda68fd6cab7150157c671c2b86eb
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-04 22:10:32 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,23 +1,22 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- * main - Entry point for writing entire lowercase alphabet in reverse
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point for writing entire lowercase alphabet in reverse
+*
+* Return: Always 0 (Success)
+*/
  int main(void)
  {
-        char ch;
-        for(ch= '0'; ch<'10'; ch++;)
-        {
-           putchar(ch);
-        }
-        for(ch='a'; ch<'g';ch++)
-        {
-        putchar(ch);
-        }
-         putchar('/n');
-         return(0);
+            char ch;
+            for(ch= '0'; ch<'10'; ch++;)
+            {
+                  putchar(ch);
+            }
+            for(ch='a'; ch<'g';ch++)
+            {
+                   putchar(ch);
+            }
+             putchar('\n');
+             return(0);
  }
