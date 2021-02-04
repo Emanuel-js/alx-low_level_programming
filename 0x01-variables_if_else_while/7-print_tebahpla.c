89279209c5b6d0086f2059e8f895219d1076d86c
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-04 22:03:40 +0300

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,8 @@
-  
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- * main - Entry point for writing entire lowercase alphabet in reverse
+* main - Entry point for writing entire lowercase alphabet in reverse
  *
  * Return: Always 0 (Success)
  */
