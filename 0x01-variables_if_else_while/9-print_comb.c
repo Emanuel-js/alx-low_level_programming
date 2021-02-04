820b8e67191ae3e260ca64315e224b7b1c70cbcd
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-04 22:18:30 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,18 +3,23 @@
 #include <stdio.h>
 
 /**
-* main - Entry point for writing entire base16 numeric set
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point for writing entire base10 single digits
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-        int n;
-        for(ch='0'; ch<='9'; ch++)
-        {
-            putchar(ch);
-            putchar(',');
-        }
-        putchar('\n');
-        return(0);
+	int n;
+
+	for (n = '0'; n <= '9' ; n++)
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
