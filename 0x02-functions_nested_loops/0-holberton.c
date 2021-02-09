e196689b664a1665d656106b75e61c96f5f35918
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-09 16:53:21 +0300

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,15 +1,19 @@
-int _putchar(char c);
+#include "holberton.h"
 /**
- * main - print Holberton.
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-	char hob[9]= "Holberton";
+	char holberton[9] = "Holberton";
 	int i;
+
 	for (i = 0; i < 9; i++)
-	 _putchar(hob[i]);
-	 _putchar('\n');
-	 return (0);
+	{
+		_putchar(holberton[i]);
+	}
+	_putchar('\n');
+	return (0);
 }
