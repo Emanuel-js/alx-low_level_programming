d226921efc74c1504c0ba484cdc68a73bb46bfb9
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-09 18:12:00 +0300

    Update holberton.h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,10 +1,15 @@
-#ifndef FILE_HOLBERTON
-#define FILE_HOLBERTON
-
-void print_alphabet(void);
-
-void print_alphabet_x10(void);
-
-void _putchar(char c);
-
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+extern int _putchar(char c);
+extern void jack_bauer(void);
+extern void print_alphabet(void);
+extern void print_alphabet_x10(void);
+extern int _islower(int c);
+extern int _isalpha(int c);
+extern int print_sign(int n);
+extern int _abs(int);
+extern int print_last_digit(int);
+extern void times_table(void);
+extern int add(int, int);
+extern void print_to_98(int n);
 #endif
