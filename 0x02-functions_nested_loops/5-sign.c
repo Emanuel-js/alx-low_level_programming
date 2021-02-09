2cf4cf46ed9a13f490af0f04d23f9798e7a86ec5
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-09 18:37:37 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,26 @@
 #include "holberton.h"
 /**
- * _isalpha - checks if letter provided is in fact a letter
- * @c: integer from which to check whether references a letter or not
+ * print_sign - checks if letter provided is in fact a letter
+ * @n: integer from which to check whether is positive, negative or zero
  * Return: Void.
  */
- 
+
 int print_sign(int n)
 {
- 
-     if(n>0)
-     {
-       _putchar('+');
-       return(1);
-     }
-     else if(n<0)
-     {
-       _putchar('-');
-       return(-1);
-     }
-     else
-     {
-       _putchar('0');
-       return(0);
-      }
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
 }
