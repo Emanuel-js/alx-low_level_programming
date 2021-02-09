dc7526077c279b8b7eb9ea8e3d088ad16409d696
Emanuel-js <etioaman1960@gmail.com>
2021-02-09 12:44:15 +0000

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include "string.h"
+/**
+ * print_alphabet - print entire alphabet
+ *
+ * Return: Void.
+ */
+
+void print_alphabet(void)
+{
+  char character;
+
+  for (character = 'a' ; character <= 'z' ; character++)
+    {
+      _putchar(character);
+    }
+  _putchar('\n');
+}
+
