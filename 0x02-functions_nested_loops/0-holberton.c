49a92811e94236c5e4f8f9d6f7d604ebfd6610a6
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-09 15:49:45 +0300

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,9 +7,8 @@
 int main(void)
 {
 	char hob[]= "Holberton";
-	int i;
-
-	for (i = 0; i < sizeof(hob); i++)
+	
+	for (int i = 0; i < sizeof(hob); i++)
 	 putchar(holberton[i]);
 	 putchar('\n');
 	 return (0);
