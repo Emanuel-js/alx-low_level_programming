82369be8f0d9c3477aaaff547fd417ee254d70ea
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-11 21:35:47 +0300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
-
 /**
- * mul -multiplies two integers.
- * @a and @b: representing num
- * Return: Return 1 if it is a digit otherwise return 0.
+ * mul - Multiply 2 integers.
+ * @a: An integer to be multiplied with b
+ * @b : An integer to be multipled with a
+ * Return: Return the result of the multiplication
  */
  int mul(int a, int b)
  {
