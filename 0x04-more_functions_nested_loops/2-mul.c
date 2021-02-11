380368ea9a796cf72e35878ebf2313d436d71167
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-11 21:37:46 +0300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -5,7 +5,7 @@
  * @b : An integer to be multipled with a
  * Return: Return the result of the multiplication
  */
- int mul(int a, int b)
- {
-    return(a*b);
- }
+int mul(int a, int b)
+{
+	return (a * b);
+}
