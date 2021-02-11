1d3a1416e6f27dfdecb59c05d2d614e58c6b2fa6
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-11 22:49:50 +0300

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *print_line - Print print line.
+ *
+ * Return: Void.
+ */
+ void print_line(int n)
+ { 
+    int i;
+    
+   for(i = 0; i < n; i++)
+   {
+            _putchar("_");
+   }
+   _putchar('\n');
+ 
+ }
