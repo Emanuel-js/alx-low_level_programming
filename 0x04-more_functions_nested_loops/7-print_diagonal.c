504e97bea85883ff22a7694da82d7c8a9cb49460
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-11 23:03:58 +0300

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,10 +9,20 @@
 void print_diagonal(int n)
 {
 	int i;
+	int gap;
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar('\');
+		for (gap = 0; gap < i; gap++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
 	}
-	_putchar('\n');
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
 }
