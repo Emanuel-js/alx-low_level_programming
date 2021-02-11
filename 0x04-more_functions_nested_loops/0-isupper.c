03443d407c18b7ee05475e81be5973be34003cf0
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-11 20:41:32 +0300

    Create 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+ 
+ int _isupper(int c)
+ {
+  if(c >= 'A' && c <= 'Z')
+    {
+        return(1);
+    }
+    else
+    {
+       return(0);
+    }
+    return 0;
+ }
