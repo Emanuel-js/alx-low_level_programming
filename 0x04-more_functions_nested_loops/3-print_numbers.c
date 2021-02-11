210228002367cfff7541c35d5d3a4b0d4d3c9d67
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-11 21:42:50 +0300

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints the numbers.
+ * 
+ * Return: Return the result of the multiplication
+ */
+ void print_numbers(void)
+ {
+    int n;
+    for(i = 0; i < 10; i++)
+    {
+        _putchar(i);
+    }
+    _putchar('\n');
+ }
