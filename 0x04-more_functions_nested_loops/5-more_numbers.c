12f1ed2a80c7e6d04c9f18571b3899325085eefe
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-11 22:18:51 +0300

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * more_numbers -  prints 10 times the numbers from 0 to 14.
+ *
+ * Return: Void.
+ */
+ void more_numbers(void)
+ {   
+     int i,j;  
+     
+     for(i i=0; i<10; i++)
+     {
+  
+          for(let j=0; j<14; j++)
+          {
+                   _putchar(j); 
+          }
+          _putchar('\n');
+     }
+     _putchar('\n');
+ }
