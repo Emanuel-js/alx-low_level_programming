e180d17dbe74c13132a17dcbdbcead100fbfeb4f
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-11 21:34:48 +0300

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * mul -multiplies two integers.
+ * @a and @b: representing num
+ * Return: Return 1 if it is a digit otherwise return 0.
+ */
+ int mul(int a, int b)
+ {
+ return(a*b);
+ }
