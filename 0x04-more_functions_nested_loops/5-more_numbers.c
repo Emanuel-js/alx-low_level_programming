99271dcc85cc55be67f747c1e0e7305f75052ce7
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-11 22:20:06 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,12 +7,13 @@
  */
  void more_numbers(void)
  {   
-     int i,j;  
+     int i;
+     int j;
      
-     for(i i=0; i<10; i++)
+     for(i = 0; i < 10; i++)
      {
   
-          for(let j=0; j<14; j++)
+          for(j = 0; j < 14; j++)
           {
                    _putchar(j); 
           }
