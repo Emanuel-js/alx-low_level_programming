32ef8e7ff7395ab5004b57461dc947ed5f78a34c
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-12 14:19:18 +0300

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,28 +1,19 @@
-#include "holberton.h"
+#include <stdio.h>
+
 /**
  * main - prints the largest prime factor of 612852475143
  * Return: always 0
  */
+
 int main(void)
 {
-	long int n=612852475143;
-	long int div=2,ans=0,maxFact;
-	
-	while(n != 0)
+	unsigned long int x, y = 612852475143;
+
+	for (x = 3; x < 782849; x = x + 2)
 	{
-		if(n % div != 0)
-		 div =div +1;
-		else
-		{
-		  maxFact=n;
-		  n=n/div;
-			if(n == 1)
-			{
-			   _putchar(maxFact);
-			    ans=1;
-			    break;
-                         }
-		}
+		while ((y % x == 0) && (y != x))
+			y = y / x;
 	}
-return(0);
+	printf("%lu\n", y);
+	return (0);
 }
