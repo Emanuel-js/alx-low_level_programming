dcea9fba218b9a1e17af4fdb46632899ee5d489f
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-16 21:15:20 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,18 +10,5 @@
 
 int main(void)
 {
-	int myrand;
-	int count;
-	int total;
-
-	srand(time(NULL));
-	for (count = 0, total = 2772; total > 122; count++)
-	{
-		//myrand = (rand() % 125) + 1;
-		//printf("%c", myrand);
-		total -= myrand;
-	}
-	printf("%c", total);
-
-	return (0);
+	
 }
