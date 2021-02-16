c4fba26cbb72393a128d7e8881c77fea5a242c0d
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-16 10:16:40 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,12 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ *void reset_to_98 is a function that takes a pointer to an int as parameter and updates the value it points to to 98.
  *
- * Return: Always 0.
+ **n is a pointer
+ * 
  */
 void reset_to_98(int *n)
 {
-  int p;
-  p=98;
-  n=&p;
-  
+  *n=98;
 }
