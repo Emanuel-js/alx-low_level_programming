2d933406b19c42e17dd59c33f3a141cc6dd45f65
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-16 12:29:57 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 
+
 /**
- * rev_string - reverse a string
- * @s: string variable
- * Return (0)
+ * rev_string - Reverse String
+ * @s: Pointer to string to be reversed
+ * Return: Always 0.
  */
 void rev_string(char *s)
 {
@@ -11,19 +12,17 @@ void rev_string(char *s)
 	int x = 0;
 	int temp;
 
-	while(s[c] !='\0')
-    {
-        c++;
-    }
+	while (s[c] != '\0')
+	{
+		c++;
+	}
 
 	c--;
 
-    while (c > x)
+	while (i > x)
 	{
 		temp = s[c];
 		s[c--] = s[x];
 		s[x++] = temp;
 	}
-
-	putchar('\n');
 }
