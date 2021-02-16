05e8f4bb5ca502b1a5f5bd7328ca67b2686957cb
Emanuel-js <etioaman1960@gmail.com>
2021-02-16 08:22:58 +0000

    update

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen - Count the length of a string
+ * @s: String whose value is to be counted
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+  int c;
+
+  while (*(s + c))
+    {
+      c++;
+    }
+  return (c);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _puts - Print entire string
+ * @str: String to be printed
+ * Return: Always 0.
+ */
+
+void _puts(char *str)
+{
+  int c;
+
+  while (*(str + c))
+    {
+      _putchar(str[c]);
+      c++;
+    }
+  _putchar('\n');
+}
