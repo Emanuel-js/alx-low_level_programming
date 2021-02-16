efe1c917ce3057c5af326e2871a5542c3056daf0
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-16 10:37:56 +0300

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
+
 /**
  * swap_int - Mirror the values of the 2 provided integers.
  * @a: Int whose value is to be exchanged with b
  * @b: Int whose value is to be exchanged with a
  * Return: Always 0.
  */
- void swap_int(int *a, int *b)
+void swap_int(int *a, int *b)
 {
-    int temp = *a;
-	 
-    *a = *b;
-    *b = temp;
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
 }
+
