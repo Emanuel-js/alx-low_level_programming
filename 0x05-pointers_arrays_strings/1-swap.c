8580606e93671dd4287574b3a5cb0f5927c4c20c
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-16 10:30:19 +0300

    Create 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - Reset int provided using pointers to 98.
+ * @n: Int whose value is to be replaced to 98
+ * Return: Always 0.
+ */
+ void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
