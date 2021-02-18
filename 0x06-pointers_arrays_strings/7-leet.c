db85e5a38b0399944d03e739b33c1fa96488346a
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-18 20:41:39 +0300

    Create 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: string variable
+ * Return: str
+ */
+char *leet(char *str)
+{
+	int i, j;
+
+	char lower[] = "aeotl";
+	char upper[] = "AEOTL";
+	char numbers[] = "43071";
+
+	while(str[i] != '\0')
+	{
+		for (j = 0; lower[j] != '\0' && upper[j] != '\0'; j++)
+		{
+			if (str[i] == lower[j] || str[i] == upper[j])
+			{
+				str[i] = numbers[j];
+			}
+		}
+	}
+	return (str);
+}
