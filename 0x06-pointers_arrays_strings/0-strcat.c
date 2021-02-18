f49f941d785d75db2452c13ee7ef2652d9f720d9
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-18 16:09:51 +0300

    concatenates

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,8 +8,8 @@
  */
 char *_strcat(char *dest, char *src)
 {
-       int i,j;
-       for(i = 0; dest[i] != '\0'; ++i)
+       int i , j;
+       for(i = 0; dest[i] != '\0'; ++i);
 
        for(j = 0; src[j] != '\0'; ++j, ++i)
        {
