c91c06dff3011821d5770ca5ee52cf6f94a0f390
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-18 16:03:19 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,22 @@
-#include "hobiltone.h"
+#include "holberton.h"
+#include "stdio.h"
+/**
+ * _strcat - copy src string ontop of dest
+ * @src: Char[] to append to dest
+ * @dest: Char[] to be appended to
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+     int i,j;
+       for(i=0; dest[i]!='\0'; ++i);
+
+       for(j=0; src[j]!='\0'; ++j, ++i)
+       {
+               dest[i]=src[j];
+       }
+      
+       dest[i]='\0';
+
+      return (dest);
+}
