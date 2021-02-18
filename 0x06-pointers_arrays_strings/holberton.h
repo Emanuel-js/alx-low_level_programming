439551b528911f07bf8c49b91a1b8d26afed02b2
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-18 22:43:43 +0300

    Update holberton.h

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,15 +1,18 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-extern char *_strcat(char *dest, char *src); 
-extern char *_strncat(char *dest, char *src, int n);
-extern char *_strncpy(char *dest, char *src, int n);
-extern void reverse_array(int *a, int n); 
-extern char *string_toupper(char *);
-extern char *cap_string(char *);
-extern char *leet(char *);
-extern char *rot13(char *);
-extern void print_number(int n);
-extern char *infinite_add(char *n1, char *n2, char *r, int size_r);
-extern void print_buffer(char *b, int size);
-#endif
 
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
+#endif
