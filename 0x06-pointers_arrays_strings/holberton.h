643b327e4fcf5153ed250889d09d5bc94461cd4c
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-18 20:48:43 +0300

    Update holberton.h

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,11 +1,15 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-extern char *_strcat(char *dest, char *src); 
-extern char *_strncat(char *dest, char *src, int n);
-extern char *_strncpy(char *dest, char *src, int n);
-extern void reverse_array(int *a, int n); 
-extern char *string_toupper(char *);
-extern char *cap_string(char *);
-extern char *leet(char *);
-extern char *rot13(char *);
-#endif
+#include <stdio.h>
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *str);
+char *cap_string(char *str);
+char *leet(char *str);
+char *rot13(char *str);
+
+#endif 
