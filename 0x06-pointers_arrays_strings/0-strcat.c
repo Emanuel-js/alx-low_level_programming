e95de3d28a49101ef809cf1cb4870a502ac2952d
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-18 16:27:06 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,7 +14,7 @@ char *_strcat(char *dest, char *src)
        }
        for (j = 0; src[j] != '\0'; ++j, ++i)
        {
-                 dest[i] = src[j];
+              dest[i] = src[j];
        }
        dest[i] = '\0';
        return (dest);
