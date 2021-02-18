51e56e3a9c5bd8effbc0af6bebdcbac72029bec8
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-18 21:37:58 +0300

    Create 8-rot13.c

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * rot13 - Encodes a string using rot13
+ *
+ * @str: String to be encoded
+ *
+ * Return: Always 0.
+ */
+char *rot13(char *str)
+{
+	int i = 0, j;
+	char s[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s1[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (str[i] != '\0')
+	{
+		for (j = 0; j <= 51; j++)
+		{
+			if (s[j] == str[i])
+			{
+				str[i] = s1[j];
+			}
+		}
+    i++;
+	}
+	return (str);
+}
