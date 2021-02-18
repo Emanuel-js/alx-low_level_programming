983b1a3d85d4e40a020921a9d4029e150f0bdaee
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-18 18:45:59 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,24 +9,21 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	 int result, i = 0;
-      
-         while (s1[i] == s2[i] && s1[i] != '\0')
-                i++;
-	
-	 if (s1[i] > s2[i])
-	 {
-		 result = s1[i] - s2[i];
-		 return (result);
-	 }
+    int result, i = 0;
+           
+    while (s1[i] == s2[i] && s1[i] != '\0')
+           i++;
+    if (s1[i] > s2[i])
+    {
+        result = s1[i] - s2[i];
+        return(result);
+    }
 
-	 else if (s1[i] < s2[i])
-	 {
-		 result = s1[i] - s2[i];
-		 return (result);
-	 }
-         else
-	     return 0;
+    else if (s1[i] < s2[i])
+    {
+        result = s1[i] - s2[i];
+        return(result);
+    }
 
-	 return 0;
+    return 0;
 }
