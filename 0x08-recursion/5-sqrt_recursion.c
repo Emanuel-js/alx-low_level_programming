1460ae333519e7c652e0096cf3772513ee36ef99
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-24 19:35:45 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,43 +1,40 @@
 #include "holberton.h"
 
-int is_square_root(int i, int j);
-
 /**
- * is_squre_root - determines the square root of
- * @j: an input value
- * @i: input value
+ * _sqrt - Finds the natural square root of an inputted number.
+ * @num: The number to find the square root of.
+ * @root: The root to be tested.
  *
- * Description: determines the square root
- * Return: returns the input val
+ * Return: If the number has a natural square root - the square root.
+ *         If the number does not have a natural square root - -1.
  */
-
-int is_square_root(int i, int j)
+int _sqrt(int num, int root)
 {
-	if (j * j > i)
-	{
+	if ((root * root) == num)
+		return (root);
+
+	if (root == num / 2)
 		return (-1);
-	}
-	if (j * j == i)
-	{
-		return (j);
-	}
-	return (is_square_root(i, j + 1));
+
+	return (_sqrt(num, root + 1));
 }
 
 /**
- * _sqrt_recursion - returns square root
- * @n: input size
+ * _sqrt_recursion - function returns the natural square root of a number
+ * @n: varaible value
  *
- * Description: If n does not have a natural square root,
- * the function should return -1
- * Return: returns the natural square root of a number
+ * Return: -1 if no natural square root
  */
-
 int _sqrt_recursion(int n)
 {
+	int root = 0;
+
 	if (n < 0)
-	{
 		return (-1);
-	}
-	return (is_square_root(n, 1));
+
+	if (n == 1)
+		return (1);
+
+	return (_sqrt(n, root));
 }
+
