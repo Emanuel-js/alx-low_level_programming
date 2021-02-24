8e54ae71d71b58001b7de6884cdaa24abde8f4d7
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-24 19:17:26 +0300

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: value x
+ * @y: value y
+ *
+ * Return: value of x
+ */
 int _pow_recursion(int x, int y)
 {
-	int answer = x;
+	int pow = x;
 
 	if (y < 0)
 	{
@@ -12,7 +21,8 @@ int _pow_recursion(int x, int y)
 	}
 	else
 	{
-		answer *= _pow_recursion(x, y - 1);
+		pow *= _pow_recursion(x, y - 1);
 	}
-	return (answer);
+	return (pow);
 }
+
