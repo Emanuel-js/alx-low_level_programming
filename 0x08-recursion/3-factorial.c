5c4a5ff64d1d3dab82fbf966dbf7048556bff32a
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-24 19:07:48 +0300

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,17 +8,16 @@
  */
 int factorial(int n)
 {
-       if (n < 0)
-       { 
-              return (-1);
-       }
-       else if (n == 0)
-       {
-              return (1);
-       }
-       else
-       {
-              return (n * factorial(n -1));
-       }
-       return (0);
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
 }
