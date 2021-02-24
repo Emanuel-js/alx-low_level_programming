9fae35c9b347190e5e1eaf993d587d4bbf0969b4
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-24 18:12:15 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,20 @@
-#
+#include "holberton.h"
+
+/**
+ * _puts_recursion -function that prints a string, followed by a new line
+ * @s: pointer to string
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
