6b7e40b59f65333c3b91937aa13f75cf03957aa1
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-24 19:29:59 +0300

    Create 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * is_squre_root - determines the square root of
+ * @j: an input value
+ * @i: input value
+ *
+ * Description: determines the square root
+ * Return: returns the input val
+ */
+
+int is_square_root(int num, int root)
+{
+
+	if ((root * root) == num)
+		return (root);
+
+	if (root == num / 2)
+		return (-1);
+
+	return (_sqrt(num, root + 1));
+}
+
+/**
+ * _sqrt_recursion - returns square root
+ * @n: input size
+ *
+ * Description: If n does not have a natural square root,
+ * the function should return -1
+ * Return: returns the natural square root of a number
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (is_square_root(n, 1));
+}
