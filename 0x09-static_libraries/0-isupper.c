0cefd870f675dd622432fc88d683a12b9a031ed4
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-02-28 20:54:20 +0300

    Create 0-isupper.c

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: integer to be checked
+ *
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
