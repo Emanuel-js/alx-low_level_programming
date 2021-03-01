e64cf39e640b587277eb3fe59fe2f6a639d3d508
Emanuel-js <etioaman1960@gmail.com>
2021-03-01 15:22:45 +0000

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * main - a program that multiplies two numbers.
- * @argc: counts the number of command line arguments
- * @argv: an array that points to argc
- *
- * Return: 0
- */
 
+/**
+ * main -  program that multiplies two numbers
+ * @argc: counts the number of command line argument
+ * @argv: array that point to argc
+ *
+ * Return: Error and 1 on failure and 0 on success
+ */
 int main(int argc, char *argv[])
 {
-      int mul;
+       int mul;
 	
-      if (argc == 3)
-      {
-	     mul = atoi(argv[1]) * atoi(argv[2]);
-             printf("%d\n", mul);
-      }
-      else
-      {
-             printf("Error\n");
-      }
-      return (0);
+       if (argc == 3)
+       {
+	       mul = atoi(argv[1]) * atoi(argv[2]);
+               printf("%d\n", mul);
+       }
+       else
+       {
+	       printf("Error\n");
+	       return (1);
+       }
+       return (0);
 }
