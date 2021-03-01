c70fa91ee39519d5ca1d8daf1f643e185b87d786
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-03-01 17:49:42 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,9 +14,9 @@ int main(int argc , char *argv[])
   
       if (argc == 3)
       {
-          for (i = 1; i < argc; i++)
+          for (i = 0; i < argc; i++)
           {
-                mul = argv[i] * argv[i + 1];
+                mul = atoi(argv[i]) * atoi(argv[i + 1]);
                 printf("%d\n", mul);
           }
       }
