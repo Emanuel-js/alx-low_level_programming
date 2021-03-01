b776ce659dc6e9fc3135f24164bf4e4a603ad07d
Emanuel-js <etioaman1960@gmail.com>
2021-03-01 15:56:12 +0000

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,15 +12,15 @@
 int main(int argc, char *argv[])
 {
         int i,sum = 0;
-        if (args == 0)
+        if (argc == 0)
         {
               printf("0\n");
         }
         else
         {
-               for (i = 1; i < args; i++)
+               for (i = 1; i < argc; i++)
                {
-                  sum += atio(argv[i]);
+                  sum += atoi(argv[i]);
                }
         }
       	printf("%d\n", sum);
