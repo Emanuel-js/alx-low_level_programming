e1e137e3d4d5fb23c88eeb5e184a31fcadd2cdca
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-03-01 19:15:46 +0300

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,28 +11,24 @@
  */
 int main(int argc, char *argv[])
 {
-        int i,j,sum = 0;
-        
-	if (argc == 0)
-        {
-              printf("0\n");
-        }
-        else
-        {
-               for (i = 1; i < argc; i++)
-               {
-                   for (j=0; argv[i][j]; j++)
-		   {
-			  if (argv[i][j] < '0' || argv[i][j] > '9')
-                          {
-                        	 printf("Error\n");
-		         	 return (1); 
-                          }
-		   }
-                   sum += atoi(argv[i]);
-               }
-        }
-      	printf("%d\n", sum);
-        return (0);
-      
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; i++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
 }
+
