e0dd0938143ba7c62426c29fac10bd58590ff165
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-03-02 20:16:20 +0300

    allocates space in memory.

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,13 +15,16 @@ char *_strdup(char *str)
 	char *duplicate;
 	int index, len = 0;
 
-	if (str == NULL || duplicate == NULL)
+	if (str == NULL)
 		return (NULL);
 
 	for (index = 0; str[index]; index++)
 		len++;
 
 	duplicate = malloc(sizeof(char) * (len + 1));
+
+	if (duplicate == NULL)
+		return (NULL);
 
 	for (index = 0; str[index]; index++)
 		duplicate[index] = str[index];
