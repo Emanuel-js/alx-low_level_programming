8873a97d9ce3d8ec87126321fea2cab283fc96d7
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-03-04 17:47:38 +0300

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,17 +2,18 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - Allocates memory using malloc.
- * @b: The number of bytes to be allocated.
- *
- * Return: A pointer to the allocated memory.
+ *malloc_checked -function that allocates memory using malloc
+ *@b:Integer being allocated memory
+ *Return:Returns a pointer to the allocated memory
+ *if malloc fails, the malloc_checked function should
+ *cause normal process termination with a status value of 98
  */
+
 void *malloc_checked(unsigned int b)
 {
-	void *p = malloc(b);
+void *mem = malloc(b);
 
-	if (p == NULL)
-		exit(98);
-
-	return (p);
+if (mem == '\0')
+exit(98);
+return (mem);
 }
