f5b9cad99d6b0938e940d2708b59146961617b8b
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-03-04 17:27:05 +0300

    malloc checker

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: input character
+ *
+ * Return: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *pointer;
+
+	pointer = malloc(b);
+	if (!pointer)
+	{
+		exit(0);
+	}
+	return (pointer);
+}
