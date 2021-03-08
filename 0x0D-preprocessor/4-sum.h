5f004ac10620f0f119aa6f37b5c09b61a50f3b89
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-03-08 19:44:27 +0300

    sum

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,6 +1,6 @@
-#ifndef SUM_VALUE_H
-#define SUM_VALUE_H
+#ifndef SUM_TWO_DIGITS_H
+#define SUM_TWO_DIGITS_H
 
-#define SUM(x,y) ((x) + (y))
+#define SUM(x, y) ((x) + (y))
 
 #endif /* ifndef SUM */
