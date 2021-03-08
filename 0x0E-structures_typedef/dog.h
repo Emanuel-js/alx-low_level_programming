a863a9dd74804e5c12b7451067a54ad75577dd80
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-03-08 19:57:53 +0300

    header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,11 +8,21 @@
  * @owner: variable of type char
  *
  */
-struct dog {
-char *name;
-float age;
-char *owner;
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* ifndef DOG_H*/
