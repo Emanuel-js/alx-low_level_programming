591cd274560a819f4de399b6d419cbeac1a76861
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-03-08 19:56:25 +0300

    header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,18 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef DOG_H
+#define DOG_H
 
+/**
+ * struct dog - new type struct dog
+ * @name: variable of type char
+ * @age: variable of type float
+ * @owner: variable of type char
+ *
+ */
 struct dog {
 char *name;
 float age;
 char *owner;
 };
+
 
 #endif
