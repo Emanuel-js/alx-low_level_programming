292d2ec3e966c34afa428a44a7c7b458dc82e15e
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-03-08 20:01:14 +0300

    init

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - Initializes a variable of type struct dog.
+ * @d: pointer variable
+ * @name: variable one
+ * @age: variable two
+ * @owner: variable three
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		(*d.name)= name;
+		(d*age) = age;
+		d->owner = owner;
+	}
+}
