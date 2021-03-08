88d48aaa31f5077dc2c27ccf72fd2dc7ebe3d0d9
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-03-08 19:33:33 +0300

    file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main()
+
+/**
+ * main - program that prints the name of the file it was compiled from
+ *        followed by a new line
+ *
+ * Return: always 0
+ */
+int main(void)
 {
-     printf("%s",__FILE__);   
+	printf("%s\n", __FILE__);
+	return (0);
 }
