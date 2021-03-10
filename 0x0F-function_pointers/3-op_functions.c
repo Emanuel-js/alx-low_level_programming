aebcdc166395dfb932ce80e11be21a6a9eacbad7
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-03-10 23:35:06 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,11 +1,10 @@
 #include "3-calc.h"
 
 /**
- * op_add - add two numbers
- * @a: first number
- * @b: second number
- *
- * Return: sum
+ * op_add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of two integers
  */
 
 int op_add(int a, int b)
@@ -14,11 +13,10 @@ int op_add(int a, int b)
 }
 
 /**
- * op_sub - difference of two numbers
- * @a: first number
- * @b: second number
- *
- * Return: difference
+ * op_sub - subtracts two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: difference of two integers
  */
 
 int op_sub(int a, int b)
@@ -27,37 +25,47 @@ int op_sub(int a, int b)
 }
 
 /**
- * op_mul - multiply two numbers
- * @a: first number
- * @b: second number
- *
- * Return: multiple
+ * op_mul - multiply two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: product of two integers
  */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - division of two numbers
- * @a: first number
- * @b: second number
- *
- * Return: division of intergers
+ * op_div - divides two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: quotient of two integers
  */
+
 int op_div(int a, int b)
 {
+	if (!b)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 
 /**
- * op_mod - modulo of two numbers
- * @a: first number
- * @b: second number
- *
- * Return: modulo of two numbers
+ * op_mod - calculates modulus two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: modulus of two integers
  */
+
 int op_mod(int a, int b)
 {
+	if (!b)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
