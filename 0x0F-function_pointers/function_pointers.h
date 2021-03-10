8111c0e9f48854c667315f85b9c33b837c402b6f
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-03-10 22:57:04 +0300

    prototype

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,10 @@
-ifndef FUNCTIONPOINTER
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
 
-ifend
+#endif /* ifndef FUNCTION_POINTERS_H */
