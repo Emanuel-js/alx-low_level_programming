c28b67245172030308a20219b6b8157039dc4eab
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-03-10 23:26:52 +0300

    Create 3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: multiple
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: division of intergers
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: modulo of two numbers
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
