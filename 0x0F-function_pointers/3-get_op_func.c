588ed2276c5d462101396712d2d8ebba66bcbfb2
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-03-10 23:33:08 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,14 +1,12 @@
 #include "3-calc.h"
-#include <stdlib.h>
 
 /**
- * get_op_func - Selects the correct function to perform
- *               the operation asked by the user.
- * @s: The operator passed as argument.
+ * get_op_func - select correct function for exe
+ * @s: input params
  *
- * Return: A pointer to the function corresponding
- *         to the operator given as a parameter.
+ * Return: 0 for code succes
  */
+
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
@@ -21,8 +19,13 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (i < 5)
+	{
+		if (!strcmp(ops[i].op, s))
+		{
+			return (ops[i].f);
+		}
 		i++;
-
-	return (ops[i].f);
+	}
+	return (NULL);
 }
