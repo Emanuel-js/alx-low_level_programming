0b287383b808b4bb91a290399a2cf9b133751b7b
Emanuel-js <etioaman1960@gmail.com>
2021-03-18 17:10:17 +0000

    Function Print list of node

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,9 +13,9 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str ==  NULL)
-			printf("%s\n", "[0] (nil)");
+			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s \n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		count++;
 	}
