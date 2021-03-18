8ead152a8b4c727825b593f86d155db249b96172
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-03-19 00:04:33 +0300

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,11 @@
 global main
+extern printf
 
 	section .text
 main:
 	  mov rax, 1
 	  mov rdi, 1
+	  mov rdi, format
 	  mov rsi, message
 	  mov rdx, 16
 	  syscall
@@ -14,3 +16,5 @@ main:
 
 message:
 	  db "Hello, Holberton", 13
+format:
+          db "%s", 10, 0	  
