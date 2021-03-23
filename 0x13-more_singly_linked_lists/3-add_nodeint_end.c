24a4967286d2ca299feb43734e84c54d9d7bb65a
Emanuel-js <etioaman1960@gmail.com>
2021-03-23 18:28:54 +0000

    Function add node to end list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,7 +15,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (!head || !new_node)
 		return (NULL);
 	new_node->n = n;
-	new_node->next= NULL;
+	new_node->next = NULL;
 	if ((*head) == NULL)
 		(*head) = new_node;
 	else
