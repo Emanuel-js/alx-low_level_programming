197ec4989f18acfcaea61a0a1990222a27f66b4b
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-03-23 21:38:24 +0300

    Function add node list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,7 +12,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
-	new = malloc(sizeof(list_t));
+	new = malloc(sizeof(listint_t));
 	if (!new || !head)
 		return (NULL);
 	new->n = n;
