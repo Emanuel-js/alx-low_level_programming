25bf9bda9ce78dbf348142f49dbe51d9f22a8600
Emanuel-js <etioaman1960@gmail.com>
2021-03-23 17:05:06 +0000

    Function Print list of node

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ *print_listint - Function that prints all the elements of a list_t list.
+ *
+ *@h: header pointer.
+ *
+ *Return:  the number of nodes.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%i \n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
