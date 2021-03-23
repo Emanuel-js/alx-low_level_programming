c49a0c2442078b8d0428a2cbf4ecbacf4bc8d95d
Emanuel-js <etioaman1960@gmail.com>
2021-03-23 18:03:32 +0000

    Function add node to end

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - adds a new node at theend of a listint_t list.
+ * @head: head node
+ * @n: number of input nodes
+ * Return: address of the new element
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *current_node;
+
+	if (!head || !new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->next= NULL;
+	if ((*head) == NULL)
+		(*head) = new_node;
+	else
+	{
+		current_node = (*head);
+		while (current_node->next != NULL)
+			current_node = current_node->next;
+		current_node->next = new_node;
+	}
+	return (new_node);
+
+}
