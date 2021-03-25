7df0cd1eeb1b51e440bfbdbfd6d9fdc07c824e6a
Emanuel-js <etioaman1960@gmail.com>
2021-03-25 18:41:50 +0000

    Convert Binery To base 10

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,19 +9,19 @@
 unsigned int binary_to_uint(const char *b)
 {
 
-	unsigned int decimal =0;
-
+	unsigned int decimal = 0;
 	int i;
-	if(!b)
-		return 0;
 
-	for(i = 0; b[i] !='\0'; ++i)
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; ++i)
 	{
-		if(b[i] !='1' && b[i] != '0')
-			return 0;
-		decimal <<= 1;
+		if (b[i] != '1' && b[i] != '0')
+			return (0);
+		decimal << = 1;
 		if (b[i] == '1')
 			decimal += 1;
 	}
-	return decimal;
+	return (decimal);
 }
