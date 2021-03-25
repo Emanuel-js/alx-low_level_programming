f5d2de8a28591c2f3831bf5a3d7e687c1b4178b2
Emanuel-js <etioaman1960@gmail.com>
2021-03-25 18:36:11 +0000

    Function convert binery to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ *binary_to_uint-function that converts a binary number to an unsigned int
+ *@b:A pointer that points to the sctring containing binary numbers
+ *Return:unsigned int with decimal value of binsry number, or 0 if error
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+
+	unsigned int decimal =0;
+
+	int i;
+	if(!b)
+		return 0;
+
+	for(i = 0; b[i] !='\0'; ++i)
+	{
+		if(b[i] !='1' && b[i] != '0')
+			return 0;
+		decimal <<= 1;
+		if (b[i] == '1')
+			decimal += 1;
+	}
+	return decimal;
+}
