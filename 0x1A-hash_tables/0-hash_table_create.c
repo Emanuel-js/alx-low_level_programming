548539f0374ac5ea1f5bd16544c18e6a51bd1ba9
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-05-28 22:18:22 +0300

    Create 0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table.
+ * @size: is the size of the array
+ * Return: a pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table = NULL;
+
+	if (size == 0)
+		return (NULL);
+
+	new_table = calloc(1, sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
+
+	new_table->size = size;
+	new_table->array = calloc(size, sizeof(hash_node_t **));
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+
+	return (new_table);
+}
