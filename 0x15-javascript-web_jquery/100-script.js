5cd7c37380c3e70d004f9e6cd3c1479155ed59e7
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-09-22 21:29:20 +0300

    Create 100-script.js

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    const text = document.querySelector('header');
+    text.style.color = "#FF0000";
+    console.log("hi");
+  });
